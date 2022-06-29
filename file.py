20480cd034a0430bfba59fe2e0a5398f92940dc6
Mukeshkannan D <106548923+mukeshinchan@users.noreply.github.com>
2022-06-29 23:17:10 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,6 +1,7 @@
 import pandas as pd 
+import streamlit as st
 
 data=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
 
 df=pd.DataFrame(data)
-print(df)
+st.texr(df)
