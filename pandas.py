51f75076003e59052785c16de010d802fc6b9ade
Mukeshkannan D <106548923+mukeshinchan@users.noreply.github.com>
2022-06-30 10:05:23 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import streamlit as st
+
+st.title('COVID-19')
 df=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
 new_df = df.melt(id_vars=['Country/Region','Province/State','Lat','Long'])
 new=new_df[new_df['Country/Region']=='India'].sort_values(by=['value'])
