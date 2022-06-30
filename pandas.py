70600f12ac0ad1148a6e5e1c13844c24d63500eb
Mukeshkannan D <106548923+mukeshinchan@users.noreply.github.com>
2022-06-30 09:57:04 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,3 +1,3 @@
-import pandas 
-my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-streamlit.dataframe(my_fruit_list)
+import pandas as pd
+df=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+streamlit.dataframe(df)
