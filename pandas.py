86239e01804756bdcc6e5c8c61ef66542124e869
Mukeshkannan D <106548923+mukeshinchan@users.noreply.github.com>
2022-06-30 09:58:18 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,3 +1,4 @@
 import pandas as pd
+import streamlit as st
 df=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
-streamlit.dataframe(df)
+st.dataframe(df)
