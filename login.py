a8771276119f29b07018abcd2665ea371e138e3f
mukeshinchan <mukeshkannanduraisamy@gmail.com>
2022-07-03 01:03:28 +0530

    image

--- login.py
+++ login.py
@@ -20,9 +20,8 @@ confrimed_covid = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/mas
 confrimed_df = pd.read_csv(confrimed_covid)
 
 new_df = confrimed_df.melt(id_vars=['Country/Region','Province/State','Lat','Long'])
+st.image('https://media-exp1.licdn.com/dms/image/C560BAQE6FPEz390mzA/company-logo_200_200/0/1645521378225?e=2147483647&v=beta&t=0Fco09XDTCU3sEQdH03z7iiK4xLQO-Ybm8Vmyn1tbEY',width=800)
 st.title('GWC - DASHBOARD')
-
-
 st.title("COVID-19 ANALIYSE")
 
 menu=['HOME','LOGIN','SIGN-UP']
@@ -60,27 +59,25 @@ elif choice=='LOGIN':
                 fig = px.line(df_selectedCountry,x = 'variable',y = 'Daily_Case',)
                 today = datetime.date.today()
                 st.title(today)
+                today_case=list(df_selectedCountry['Daily_Case'])
+                st.header(f'Coronavirus LIVE Updates: {selectedCountry} Records {today_case[-1]} New COVID-19 Cases In Last 24 Hours')
                 st.plotly_chart(fig)
-                
-                new=new_df[new_df['Country/Region']=='India'].sort_values(by=['value'])
-                a=[nn-n for n,nn in zip(new['value'],new['value'][1:]+[0])]
-                a.insert(0,0)
-                new['Cum-sum']=a
-                daily=pd.DataFrame(new[['value','Cum-sum']])
-                fig2=px.pie(daily)
-                st.plotly_chart(fig2)
-                st.table(df_selectedCountry)
+                st.table(df_selectedCountry.tail(5))
             if page_value == 'Deaths':
+                country_list = list(new_df['Country/Region'].unique())
+                selectedCountry2 = st.sidebar.selectbox('Select Country', country_list)
                 death_df=pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
                 death_pvt_df=death_df.melt(id_vars=['Province/State','Country/Region','Lat','Long'],var_name='Date',value_name='RUNNING_TOTAL')
-                new_2=death_pvt_df[death_pvt_df['Country/Region']==selectedCountry].sort_values(by=['RUNNING_TOTAL'])
+                new_2=death_pvt_df[death_pvt_df['Country/Region']==selectedCountry2]
                 b=[nn-n for n,nn in zip(new_2['RUNNING_TOTAL'],new_2['RUNNING_TOTAL'][1:]+[0])]
                 b.insert(0,0)
                 new_2['Cum-sum']=b
                 st.table(new_2.tail(1))
-                l=new_2[new_2['Country/Region']==selectedCountry]
+                l=new_2[new_2['Country/Region']==selectedCountry2]
                 cum=px.line(l['Cum-sum'])
                 runn=px.line(l['RUNNING_TOTAL'])
+                today_deaths=list(new_2['Cum-sum'])
+                st.header(f'Coronavirus LIVE Updates: {selectedCountry2} Records {today_deaths[-1]} New COVID-19 Cases In Last 24 Hours')
                 st.plotly_chart(cum)
                 st.plotly_chart(runn)
         else:
