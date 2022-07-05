7f93f2b04c05b2bb551109542c4751adb6d24a76
mukeshinchan <mukeshkannanduraisamy@gmail.com>
2022-07-05 12:26:04 +0530

    TABLEAU

--- login.py
+++ login.py
@@ -7,6 +7,7 @@ import sqlite3
 connect=sqlite3.connect('data.db')
 c=connect.cursor()
 import datetime
+import streamlit.components.v1 as components
 
 def create_table():
     c.execute('CREATE TABLE IF NOT EXISTS user(username TEXT , password TEXT)')
@@ -30,8 +31,41 @@ with col2:
 st.image('https://www.usda.gov/sites/default/files/covid-header-2.png')
 st.title("COVID-19 ANALYSE")
 st.write('A COVIDSafe Plan is a list of health and safety actions. It is an important part of the occupational health and safety obligations of every workplace and is required under the Victorian Government’s pandemic orders.Every Victorian business or organisation with on-site operations must keep a COVIDSafe Plan at each workplace. They must provide it to an Authorised Officer upon request and comply with any direction given by an authorised officer or WorkSafe inspector to modify the COVIDSafe plan. The plan must set out how the workplace will keep workers, customers and other attendees safe from COVID-19. It also helps to prepare for a case of COVID-19 in the workplace.All workplaces should regularly review and update their COVIDSafe Plan to ensure it reflects the current COVIDSafe settings. A COVIDSafe Plan template has been developed to assist workplaces. However, it is not mandatory to use this template.')
+components.html(
+    """
+     <div class='tableauPlaceholder' id='viz1656936464205' style='position: relative'><noscript><a href='#'>
+<img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;co&#47;covid19_16386879014100&#47;Dashboard1&#47;1_rss.png' style='border: none' />
+</a>
+</noscript>
+<object class='tableauViz'  style='display:none;'>
+<param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> 
+<param name='embed_code_version' value='3' /> 
+<param name='site_root' value='' />
+<param name='name' value='covid19_16386879014100&#47;Dashboard1' />
+<param name='tabs' value='yes' /><param name='toolbar' value='yes' />
+<param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;co&#47;covid19_16386879014100&#47;Dashboard1&#47;1.png' /> 
+<param name='animate_transition' value='yes' />
+<param name='display_static_image' value='yes' />
+<param name='display_spinner' value='yes' />
+<param name='display_overlay' value='yes' />
+<param name='display_count' value='yes' />
+<param name='language' value='en-US' />
+</object></div>                
+<script type='text/javascript'>                    
+var divElement = document.getElementById('viz1656936464205');                    
+var vizElement = divElement.getElementsByTagName('object')[0];                    
+if ( divElement.offsetWidth > 800 ) { vizElement.style.minWidth='820px';vizElement.style.maxWidth='100%';vizElement.style.minHeight='810px';vizElement.style.maxHeight=(divElement.offsetWidth*0.75)+'px';} 
+else if ( divElement.offsetWidth > 500 ) { vizElement.style.minWidth='1220px';vizElement.style.maxWidth='100%';vizElement.style.minHeight='810px';
+vizElement.style.maxHeight=(divElement.offsetWidth*0.75)+'px';} 
+else { vizElement.style.width='100%';vizElement.style.minHeight='800px';vizElement.style.maxHeight=(divElement.offsetWidth*1.77)+'px';}                     var scriptElement = document.createElement('script');        
+scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+vizElement.parentNode.insertBefore(scriptElement, vizElement);               
+</script>
+    """,
+    height=1000, width=1200
+)
 
-menu=['HOME','LOGIN','SIGN-UP']
+menu=['TABLEAU PERVIVEW YEARS','LOGIN','SIGN-UP']
 choice=st.sidebar.selectbox('MENU',menu)
 if choice == 'HOME':
     st.subheader('HOME')
